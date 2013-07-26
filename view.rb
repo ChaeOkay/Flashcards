483cdca3beb73e453b0acce7b94030933270a2bf
cokeefe <chaemobile@gmail.com>
2013-07-26 12:03:43 -0700

    add view

---
+++ view.rb
@@ -0,0 +1,20 @@
+require_relative 'model'
+
+class View
+  def self.welcome
+    # puts welcome message
+  end
+
+  def self.exit
+    # puts exit message
+  end
+
+  def self.get_guess
+  end
+
+  def self.correct_card
+  end
+
+  def self.incorrect_card
+  end
+end
